df690c4f4bd817742876ec031faeb838b44c9864
cse-471 <munawarmahtabansary@gmail.com>
2024-04-20 03:31:03 +0600

    rectified mail

--- CarRentalSystem/settings.py
+++ CarRentalSystem/settings.py
@@ -31,8 +31,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'headspace799@gmail.com'
-EMAIL_HOST_PASSWORD = 'qljrrqdkmybqkqve'
+EMAIL_HOST_USER = 'quicktrips178@gmail.com'
+EMAIL_HOST_PASSWORD = 'tmdfdjjiugypkecf'
 
 
 ALLOWED_HOSTS = []
